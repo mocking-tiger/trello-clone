929a110d556691ad6922b344407eb29db77161fc
Park JunYong <dyden119@gmail.com>
2024-12-30 22:50:37 +0900

    chore: recoil set함수 연습

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,36 @@
 import "./App.css";
+import { useRecoilState } from "recoil";
+import { hourSelector, minuteState } from "./atoms";
+import { FormEvent } from "react";
 
 function App() {
-  return <div>프로젝트 초기화</div>;
+  const [minutes, setMinutes] = useRecoilState(minuteState);
+  const [hours, setHours] = useRecoilState(hourSelector);
+
+  const onMinutesChange = (event: FormEvent<HTMLInputElement>) => {
+    setMinutes(+event.currentTarget.value); // string에 +를 붙이면 number로 형변환 됨
+  };
+
+  const onHoursChange = (event: FormEvent<HTMLInputElement>) => {
+    setHours(+event.currentTarget.value);
+  };
+
+  return (
+    <div>
+      <input
+        value={minutes}
+        onChange={onMinutesChange}
+        type="number"
+        placeholder="Minutes"
+      />
+      <input
+        value={hours}
+        onChange={onHoursChange}
+        type="number"
+        placeholder="Hours"
+      />
+    </div>
+  );
 }
 
 export default App;

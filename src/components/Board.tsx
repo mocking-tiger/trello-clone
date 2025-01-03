2757870f23d9ed16e148d2f36db3122153f33113
Park JunYong <dyden119@gmail.com>
2025-01-03 17:16:01 +0900

    chore: 이동 타겟이 되는 보드 와 출처 보드 식별되게

--- src/components/Board.tsx
+++ src/components/Board.tsx
@@ -2,6 +2,11 @@ import DraggableCard from "./DraggableCard";
 import styled from "styled-components";
 import { Droppable } from "react-beautiful-dnd";
 
+interface IAreaProps {
+  isDraggingOver: boolean;
+  isDraggingFromThis: boolean;
+}
+
 const Wrapper = styled.div`
   width: 300px;
   min-height: 300px;
@@ -9,6 +14,8 @@ const Wrapper = styled.div`
   padding-top: 10px;
   background-color: ${(props) => props.theme.boardColor};
   border-radius: 5px;
+  display: flex;
+  flex-direction: column;
 `;
 
 const Title = styled.h2`
@@ -16,6 +23,13 @@ const Title = styled.h2`
   font-weight: 600;
   margin-bottom: 10px;
   font-size: 18px;
+`;
+
+const Area = styled.div<IAreaProps>`
+  background-color: ${(props) =>
+    props.isDraggingOver ? "pink" : props.isDraggingFromThis ? "red" : "blue"};
+  flex-grow: 1;
+  transition: background-color 0.3s ease-in-out;
 `;
 
 // 스타일드 컴포넌트 영역 끝
@@ -30,13 +44,18 @@ export default function Board({ toDos, boardId }: IBoardProps) {
     <Wrapper>
       <Title>{boardId}</Title>
       <Droppable droppableId={boardId}>
-        {(props) => (
-          <div ref={props.innerRef} {...props.droppableProps}>
+        {(provided, snapshot) => (
+          <Area
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+            isDraggingOver={snapshot.isDraggingOver}
+            isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+          >
             {toDos.map((toDo, index) => (
               <DraggableCard key={toDo} toDo={toDo} index={index} />
             ))}
-            {props.placeholder}
-          </div>
+            {provided.placeholder}
+          </Area>
         )}
       </Droppable>
     </Wrapper>

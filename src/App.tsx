3d42f512dc138e92e6f15b27ba44b0346995a769
Park JunYong <dyden119@gmail.com>
2025-01-05 20:44:11 +0900

    design: 버튼,모달로 새 보드 생성

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import styled from "styled-components";
 import Board, { IAreaProps, IForm } from "./components/Board";
 import CanIcon from "./assets/trash.svg";
+import AddIcon from "./assets/plus.svg";
 import { DragDropContext, Droppable, DropResult } from "react-beautiful-dnd";
 import { useRecoilState } from "recoil";
 import { toDoState } from "./atoms";
 import { useForm } from "react-hook-form";
+import { useState } from "react";
 
 const Wrapper = styled.div`
   width: 100vw;
@@ -27,8 +29,8 @@ const TrashCan = styled.div<IAreaProps>`
   width: 75px;
   height: 75px;
   position: absolute;
-  left: 50%;
-  bottom: 150px;
+  right: 5%;
+  bottom: 10%;
   background-color: ${(props) =>
     props.$isDraggingOver ? "#b2bec3" : "#dfe6e9"};
   border-radius: 50%;
@@ -44,11 +46,74 @@ const TrashCan = styled.div<IAreaProps>`
   }
 `;
 
+const AddBoardButton = styled.div`
+  width: 75px;
+  height: 75px;
+  position: absolute;
+  left: 5%;
+  bottom: 10%;
+  background-color: #dfe6e9;
+  border-radius: 50%;
+  transition: all 0.2s ease-in-out;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  &:hover {
+    transform: scale(1.2);
+    cursor: pointer;
+  }
+
+  img {
+    width: 37.5px;
+    height: 37.5px;
+  }
+`;
+
+const NewBoardModal = styled.div`
+  width: 100vw;
+  height: 100vh;
+  background-color: rgba(255, 255, 255, 0.6);
+  position: fixed;
+  top: 0;
+  left: 0;
+  z-index: 1;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  div {
+    width: 480px;
+    padding: 30px;
+    background-color: #b2bec3;
+    border-radius: 6px;
+
+    h2 {
+      text-align: center;
+      margin-bottom: 10px;
+      font-size: 20px;
+      color: white;
+    }
+
+    input {
+      width: 100%;
+      padding: 10px;
+      border: 0;
+      border-radius: 6px;
+
+      &:focus {
+        outline: none;
+      }
+    }
+  }
+`;
+
 // 스타일드 컴포넌트 영역 끝
 
 function App() {
-  const { register, setValue, handleSubmit } = useForm<IForm>();
+  const { register, setValue, handleSubmit, setFocus } = useForm<IForm>();
   const [toDos, setToDos] = useRecoilState(toDoState);
+  const [isModalOn, setIsModalOn] = useState(false);
 
   const onDragEnd = (info: DropResult) => {
     console.log(info);
@@ -97,7 +162,6 @@ function App() {
   };
 
   const createBoard = ({ boardName }: IForm) => {
-    console.log(boardName);
     setValue("boardName", "");
     setToDos((allBoards) => {
       const newBoard = {
@@ -107,17 +171,25 @@ function App() {
       localStorage.setItem("TODOS", JSON.stringify(newBoard));
       return newBoard;
     });
+    handleModal();
+  };
+
+  const handleModal = () => {
+    if (isModalOn) {
+      setIsModalOn(false);
+    } else {
+      setIsModalOn(true);
+      setTimeout(() => {
+        setFocus("boardName");
+      }, 0);
+    }
   };
 
   return (
     <DragDropContext onDragEnd={onDragEnd}>
-      <form onSubmit={handleSubmit(createBoard)}>
-        <input
-          {...register("boardName", { required: true })}
-          type="text"
-          placeholder="새 보드 생성"
-        />
-      </form>
+      <AddBoardButton onClick={handleModal}>
+        <img src={AddIcon} alt="보드 추가 아이콘" />
+      </AddBoardButton>
       <Wrapper>
         <Boards>
           {Object.keys(toDos).map((boardId) => (
@@ -125,18 +197,34 @@ function App() {
           ))}
         </Boards>
       </Wrapper>
-      <Droppable droppableId="trash">
-        {(provided, snapshot) => (
-          <TrashCan
-            ref={provided.innerRef}
-            {...provided.droppableProps}
-            $isDraggingOver={snapshot.isDraggingOver}
-          >
-            <img src={CanIcon} alt="휴지통 아이콘" />
-            <div style={{ position: "absolute" }}>{provided.placeholder}</div>
-          </TrashCan>
-        )}
-      </Droppable>
+      {Object.keys(toDos).length && (
+        <Droppable droppableId="trash">
+          {(provided, snapshot) => (
+            <TrashCan
+              ref={provided.innerRef}
+              {...provided.droppableProps}
+              $isDraggingOver={snapshot.isDraggingOver}
+            >
+              <img src={CanIcon} alt="휴지통 아이콘" />
+              <div style={{ position: "absolute" }}>{provided.placeholder}</div>
+            </TrashCan>
+          )}
+        </Droppable>
+      )}
+      {isModalOn && (
+        <NewBoardModal onClick={handleModal}>
+          <div>
+            <h2>새 보드 추가하기</h2>
+            <form onSubmit={handleSubmit(createBoard)}>
+              <input
+                {...register("boardName", { required: true })}
+                type="text"
+                placeholder="이름 입력해줘"
+              />
+            </form>
+          </div>
+        </NewBoardModal>
+      )}
     </DragDropContext>
   );
 }
